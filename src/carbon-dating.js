1feddf9b76d1880bf873a976da6172a616ff4cd3
VolodymyrStyk <volodymyr.styk@gmail.com>
2021-03-25 18:10:56 +0200

    fix: add if  counCarbon

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,14 +4,18 @@ const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  let sample = parseFloat(sampleActivity);
-  if (isNaN(sample) || sample <= 0 || sample > 15) {
+  if (typeof sampleActivity === "string") {
+    let sample = parseFloat(sampleActivity);
+    if (isNaN(sample) || sample <= 0 || sample > 15) {
+      return false;
+    }
+    if (typeof sample === "number") {
+      let k = Math.LN2 / HALF_LIFE_PERIOD;
+      let upper = Math.log(MODERN_ACTIVITY / sample);
+      console.log(Math.ceil(upper / k));
+      return Math.ceil(upper / k);
+    }
+  } else {
     return false;
-  }
-  if (typeof sample === "number") {
-    let k = Math.LN2 / HALF_LIFE_PERIOD;
-    let upper = Math.log(MODERN_ACTIVITY / sample);
-    console.log(Math.ceil(upper / k));
-    return Math.ceil(upper / k);
   }
 };
