404cfe351dc354bf91d788e43095ec361842ea4b
VolodymyrStyk <volodymyr.styk@gmail.com>
2021-03-25 19:14:15 +0200

    fix: dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,15 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-  for (let member of members) {
-    if (typeof member === "string") {
-      member = member.replace(/\s+/g, "");
-      title.push(member[0].toUpperCase());
-    } else {
-      member = "";
+  if (Array.isArray(members)) {
+    for (let member of members) {
+      if (typeof member === "string") {
+        member = member.replace(/\s+/g, "");
+        title.push(member[0].toUpperCase());
+      } else {
+        member = "";
+      }
     }
-  }
-  newTitle = title.sort().join("");
+    newTitle = title.sort().join("");
 
-  return newTitle;
+    return newTitle;
+  } else {
+    return false;
+  }
 };
