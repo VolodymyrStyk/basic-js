1e05cfbb7c6f1d97fd16336cf8d11a53a3543a53
VolodymyrStyk <volodymyr.styk@gmail.com>
2021-03-25 17:49:35 +0200

    feat: implement counCarbon

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  let sample = Math.ceil(Number(sampleActivity));
+  if (isNaN(sample)) {
+    return false;
+  }
+  if (typeof sample === "number") {
+    let k = Math.LN2 / HALF_LIFE_PERIOD;
+    let upper = Math.log(MODERN_ACTIVITY / sample);
+    console.log(Math.ceil(upper / k));
+    return Math.ceil(upper / k);
+  }
 };
